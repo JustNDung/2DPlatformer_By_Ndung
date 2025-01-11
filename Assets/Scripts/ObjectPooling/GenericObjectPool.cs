7f1a3b66525ea27cd6cd8d3056104f03dbdc4505
JustNDung <nguyenngocdung19122005@gmail.com>
2025-01-11 23:18:11 +0700

    Refactor folder structure. Add bullet behavior.

---
+++ Assets/Scripts/ObjectPooling/GenericObjectPool.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class GenericObjectPool<T> where T : MonoBehaviour
+{
+    private Queue<T> pool = new Queue<T>();
+    private T prefab;
+    private Transform parent;
+
+    public GenericObjectPool(T prefab, int initialSize, Transform parent = null)
+    {
+        this.prefab = prefab;
+        this.parent = parent;
+
+        for (int i = 0; i < initialSize; i++)
+        {
+            CreateNewObject();
+        }
+    }
+
+    private T CreateNewObject()
+    {
+        T obj = Object.Instantiate(prefab, parent);
+        obj.gameObject.SetActive(false);
+        pool.Enqueue(obj);
+        return obj;
+    }
+
+    public T Get()
+    {
+        if (pool.Count == 0)
+        {
+            CreateNewObject();
+        }
+
+        T obj = pool.Dequeue();
+        obj.gameObject.SetActive(true);
+        return obj;
+    }
+
+    public void Return(T obj)
+    {
+        obj.gameObject.SetActive(false);
+        pool.Enqueue(obj);
+    }
+}
