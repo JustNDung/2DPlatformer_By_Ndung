88928153d186a3a1d4b3bd61db3be9c18f99a9e5
JustNDung <nguyenngocdung19122005@gmail.com>
2025-01-18 14:58:15 +0700

    Implement EvilFireWizard state machine and behavior but not done yet
    Added scripts to define states (Idle, Move, Attack, Hurt, Dead) and behavior for the EvilFireWizard. Integrated the state machine with animations, player interaction, and movement logic. Updated MainScene to include EvilFireWizard with corresponding state transitions and tile modifications.

---
+++ Assets/Scripts/Enemies/EvilFireWizard/EFWDead.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class EFWDead : State
+{
+    private EFWStateMachine stateMachine;
+    public override void Enter()
+    {
+        stateMachine = GetComponent<EFWStateMachine>();
+        stateMachine.anim.SetTrigger("Dead");
+    }
+
+    public override void HandleInput()
+    {
+        
+    }
+
+    public override void LogicUpdate()
+    {
+        
+    }
+
+    public override void Exit()
+    {
+        
+    }
+}
